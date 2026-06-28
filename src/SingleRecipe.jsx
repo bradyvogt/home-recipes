0088d6385bbb62a299c131bc4cc31d37e5f8097a
bradyvogt <bradyvogt5@gmail.com>
2026-06-27 21:01:03 -0500

    Simple single recipe page

--- src/SingleRecipe.jsx
+++ src/SingleRecipe.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { queryParamToName } from './utils/helpers';
+import { Link } from 'react-router-dom';
 
 const SingleRecipe = () => {
     const searchParams = new URLSearchParams(window.location.search);
-    
+
     return (
         <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+            <Link to='/'>Back to All Recipes</Link>
+            <br /><br />
             <h1>Recipe Detail</h1>
             <p>Viewing: <strong>{queryParamToName(searchParams.get('name') || 'No Recipe Found')}</strong></p>
         </div>
